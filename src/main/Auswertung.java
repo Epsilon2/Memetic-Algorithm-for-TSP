18a6553b111429f444fbb382e6a7551666ab0768
Epsilon2 <tobias.baumann@uni-ulm.de>
2013-12-27 16:24:56 +0100

    Update Auswertung.java

--- src/main/Auswertung.java
+++ src/main/Auswertung.java
@@ -7,13 +7,15 @@ public class Auswertung {
 	public static void main(String[] args) {
 		String[] nameOfTestInstances = {"att48"};
 		int numberOfTestRuns = 2;
+		int[] timeLimits = {};
+		List<Double> results = new List<>();
 		try {
 			for(String s: nameOfTestInstances)
 			{
 				for(int i = 0; i<numberOfTestRuns; i++)
 				{
 					String[] arguments = {s, String.valueOf(5), String.valueOf(50)};
-					TSP_Main.main(arguments);
+					results.add(TSP_Main.main(arguments));
 				}
 			}
 		} catch (IOException e) {
