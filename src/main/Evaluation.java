7f30ac8a840e129c26cdd85016ff25c82f63bf05
Tobias Baumann <tobias.baumann@uni-ulm.de>
2014-01-18 12:45:02 +0100

    Updated evaluation class

--- src/main/Evaluation.java
+++ src/main/Evaluation.java
@@ -2,6 +2,7 @@ package main;
 
 import java.awt.Font;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.LinkedList;
 
@@ -15,6 +16,7 @@ import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.labels.BoxAndWhiskerToolTipGenerator;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.renderer.category.BoxAndWhiskerRenderer;
+import org.jfree.data.Range;
 import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
 
 public class Evaluation extends JFrame{
@@ -38,6 +40,7 @@ public class Evaluation extends JFrame{
 		int[] timeLimits = {1,2,15,120};
 		LinkedList<Double> ilsResults = new LinkedList<Double>();
 		LinkedList<Double> memeResults = new LinkedList<Double>();       
+
         final DefaultBoxAndWhiskerCategoryDataset dataset 
             = new DefaultBoxAndWhiskerCategoryDataset();
 		try {
@@ -49,13 +52,13 @@ public class Evaluation extends JFrame{
 					String[] arguments = {nameOfTestInstances[j], String.valueOf(timeLimits[j]), String.valueOf(50), "noDiagrams"};
 					double[] results = TSP_Main.main(arguments);
 					ilsResults.add(results[0]);
-					memeResults.add(results[1]);
-					
+					memeResults.add(results[1]);					
 				}
 				dataset.add(ilsResults, "Iterated Local Search", nameOfTestInstances[j]);
 				dataset.add(memeResults, "Memetic Algorithm", nameOfTestInstances[j]);
 				ilsResults.clear();
 				memeResults.clear();
+
 			}
 	        final CategoryAxis xAxis = new CategoryAxis("Name of TSPLIB instance");
 	        final NumberAxis yAxis = new NumberAxis("deviation from optimum in %");
@@ -66,7 +69,8 @@ public class Evaluation extends JFrame{
 	        renderer.setMeanVisible(false);
 	        renderer.setMaximumBarWidth(10);
 	        final CategoryPlot plot = new CategoryPlot(dataset, xAxis, yAxis, renderer);
-
+	        final NumberAxis axis = (NumberAxis) plot.getRangeAxis();
+	        axis.setRange(Range.expandToInclude(axis.getRange(), -0.2));
 	        final JFreeChart chart = new JFreeChart(
 	            "Comparison of ILS and Memetic Algorithm",
 	            new Font("SansSerif", Font.BOLD, 16),
@@ -75,9 +79,8 @@ public class Evaluation extends JFrame{
 	        );
 	        final ChartPanel chartPanel = new ChartPanel(chart);
 //	        chartPanel.setPreferredSize(new java.awt.Dimension(900, 540));
-	        ChartUtilities.saveChartAsJPEG(new File("C:/" + "evaluation"), chart, 500, 400);
+	        ChartUtilities.saveChartAsJPEG(new File("evaluation"), chart, 500, 400);
 	        setContentPane(chartPanel);
-			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
